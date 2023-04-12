63d59becd5d4858eb578b00bebbc308a7d140f4b
royanlord <royanfarid32@gmail.com>
2023-04-13 05:11:02 +0700

    create logic n algorithm to show result display

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,42 +3,62 @@ let inputWeight = document.getElementById("weight-field")
 let btnCalc = document.getElementById("btn-calc")
 
 
+const hitungBMI = () => {
+    let heightVal = inputHeight.value
+    let weightVal = inputWeight.value
+    let calc = weightVal / ((heightVal/100)**2)
+    rounding = calc.toFixed(1)
+    return rounding
+}
+
+const displayBMI = () => {
+    let hasilHitung = hitungBMI()
+    let firstResult = document.querySelector(".first-result")
+    let secondResult = document.querySelector(".second-result")
+    let displayDetail = document.querySelector(".detail")
+
+    firstResult.innerText = `${hasilHitung}`
+    secondResult.innerText = `${hasilHitung}`
+
+    if (hasilHitung >= 30) {
+        displayDetail.innerText = "Obesity"
+    } else if (hasilHitung <= 29.9 && hasilHitung >= 25) {
+        displayHasil.innerText = "Overweight"
+    } else if (hasilHitung <= 24.9 && hasilHitung >= 18.5) {
+        displayDetail.innerText = "Normal"
+    } else {
+        displayDetail.innerText = "Underweight"
+    }
+}
+
 const validateForm = () => {
     let heightVal = inputHeight.value
     let weightVal = inputWeight.value
 
+    let calc = document.getElementById("bmi-calc")
+    let result = document.getElementById("bmi-result")
 
-    if (heightVal === "") {
-        document.getElementById("warning-height").innerText = "Tinggi tidak boleh kosong!"
-    } else if (heightVal !== "") {
-        document.getElementById("warning-height").innerText = ""
-    }
-    if (weightVal === "") {
-        document.getElementById("warning-weight").innerText = "Berat tidak boleh kosong!"
-    } else if (weightVal !== "") {
-        document.getElementById("warning-weight").innerText = ""
+    if (heightVal !== "" && weightVal !== "") {
+        displayBMI()
+        calc.style.display = "none"
+        result.style.display = "flex"
+    } else {
+        if (heightVal === "") {
+            document.getElementById("warning-height").innerText = "Tinggi tidak boleh kosong!"
+        } else if (heightVal !== "") {
+            document.getElementById("warning-height").innerText = ""
+        }
+        if (weightVal === "") {
+            document.getElementById("warning-weight").innerText = "Berat tidak boleh kosong!"
+        } else if (weightVal !== "") {
+            document.getElementById("warning-weight").innerText = ""
+        }
     }
 }
-
-
-const hitungBMI = () => {
-    validateForm()
-    let heightVal = inputHeight.value
-    let weightVal = inputWeight.value
-    let calc = weightVal / ((heightVal/100)**2)
-    
-    console.log(calc.toFixed(1));
-}
-
-
-
-// const displayBMI = () => {
-    
-// }
 
 btnCalc.addEventListener("click", function(e) {
     e.preventDefault()
-    hitungBMI()
+    validateForm()
 
     // let heightVal = inputHeight.value
     // let weightVal = inputWeight.value
