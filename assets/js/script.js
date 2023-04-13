4ea8b01d843da6d2b0f97d0f7b22066b97732053
royanlord <royanfarid32@gmail.com>
2023-04-13 16:28:51 +0700

    create result detail n button re-check

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,6 +16,7 @@ const displayBMI = () => {
     let firstResult = document.querySelector(".first-result")
     let secondResult = document.querySelector(".second-result")
     let displayDetail = document.querySelector(".detail")
+    let listDetail = document.querySelector(".list-detail")
 
     firstResult.innerText = `${hasilHitung}`
     secondResult.innerText = `${hasilHitung}`
@@ -25,26 +26,50 @@ const displayBMI = () => {
         firstResult.style.color = "#ff4700"
         secondResult.style.color = "#ff4700"
         displayDetail.style.color = "#ff4700"
+        listDetail.innerHTML =  `
+            <li>Jaga pola makan dengan mengurangi makanan tinggi gula dan tinggi lemak untuk mencapai berat badan ideal</li>
+            <li>Tingkatkan aktivitas dan berolahraga secara teratur</li>
+            <li>Lakukan pola hidup sehat (tidak merokok, tidak minum beralkohol)</li>
+        `
     } else if (hasilHitung <= 34.9 && hasilHitung >= 30 ) {
         displayDetail.innerText = "Obesity 1"
         firstResult.style.color = "#feb800"
         secondResult.style.color = "#feb800"
         displayDetail.style.color = "#feb800"
+        listDetail.innerHTML =  `
+            <li>Jaga pola makan dengan mengurangi makanan tinggi gula dan tinggi lemak untuk mencapai berat badan ideal</li>
+            <li>Tingkatkan aktivitas dan berolahraga secara teratur</li>
+            <li>Lakukan pola hidup sehat (tidak merokok, tidak minum beralkohol)</li>
+        `
     } else if (hasilHitung <= 29.9 && hasilHitung >= 25) {
-        displayHasil.innerText = "Overweight"
+        displayDetail.innerText = "Overweight"
         firstResult.style.color = "#9ec12f"
         secondResult.style.color = "#9ec12f"
         displayDetail.style.color = "#9ec12f"
+        listDetail.innerHTML =  `
+            <li>Jaga pola makan dengan mengurangi makanan tinggi gula dan tinggi lemak untuk mencapai berat badan ideal</li>
+            <li>Tingkatkan aktivitas dan berolahraga secara teratur</li>
+            <li>Lakukan pola hidup sehat (tidak merokok, tidak minum beralkohol)</li>
+        `
     } else if (hasilHitung <= 24.9 && hasilHitung >= 18.5) {
         displayDetail.innerText = "Normal"
         firstResult.style.color = "#49b47c"
         secondResult.style.color = "#49b47c"
         displayDetail.style.color = "#49b47c"
+        listDetail.innerHTML =  `
+            <li>Pertahankan pola makan saat ini agar tetap dalam kondisi optimal</li>
+            <li>Untuk mempertahankan BMI normal, lakukan aktifitas fisik 3 kali dalam seminggu</li>
+        `
     } else {
         displayDetail.innerText = "Underweight"
         firstResult.style.color = "#4392d4"
         secondResult.style.color = "#4392d4"
         displayDetail.style.color = "#4392d4"
+        listDetail.innerHTML =  `
+            <li>Tingkatkan konsumsi protein untuk mencapai berat badan ideal</li>
+            <li>Untuk mencapai berat badan ideal kamu perlu makan makanan bergizi dan tingkatkan frekuensi makan</li>
+            <li>Tingkatkan aktivitas dan berolahraga secara teratur, dan lakukan pola hidup sehat</li>
+        `
     }
 }
 
@@ -76,10 +101,11 @@ const validateForm = () => {
 btnCalc.addEventListener("click", function(e) {
     e.preventDefault()
     validateForm()
+})
 
-    // let heightVal = inputHeight.value
-    // let weightVal = inputWeight.value
-    
-    // console.log(heightVal);
-    // console.log(weightVal);
+let reCheck = document.getElementById("re-check")
+reCheck.addEventListener("click", function(e) {
+    e.preventDefault()
+    calc.style.display = "flex"
+    result.style.display = "none"
 })
